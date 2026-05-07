6ed89279f78f17a7e143a7bbdd23952ccc5b6885
ahmedekramalsada <ahmedekramalsada@gmail.com>
2026-05-07 18:03:54 +0300

    push the repo

---
+++ demo/src/main/java/com/api/demo/DemoApplication.java
@@ -0,0 +1,25 @@
+package com.api.springapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApiApplication.class, args);
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "OK";
+    }
+
+    @GetMapping("/api/test")
+    public String test() {
+        return "Hello World!";
+    }
+}
